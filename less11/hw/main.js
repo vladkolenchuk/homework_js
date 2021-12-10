500aedfae65af62a68c4d91b391e9e3977db9254
vladkolenchuk <kolenchuk09@gmail.com>
2021-12-10 12:11:31 +0200

    ...

--- less11/hw/main.js
+++ less11/hw/main.js
@@ -30,29 +30,31 @@
 // -створити форму з інпутами для model,type та volume автівки.
 //     при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
 
-// let form1 = document.createElement('form');
-// let model = document.createElement('input');
-// let type = document.createElement('input');
-// let volume = document.createElement('input');
-// let btn1 = document.createElement('button');
-// btn1.innerText = 'Нажми мене';
-// form1.setAttribute('name','form1');
-// model.setAttribute('name', 'model');
-// type.setAttribute('name', 'type');
-// volume.setAttribute('name', 'volume');
-// document.body.appendChild(form1);
-// form1.appendChild(model);
-// form1.appendChild(type);
-// form1.appendChild(volume);
-// document.body.appendChild(btn1);
-//
-// let goLocalStorage = function (modelCar,typeCar,volumeCar){
-//     let car =[];
-//     car.push({modelCar,typeCar,volumeCar});
-//     let infoCar = 'infoCar';
-//     localStorage.setItem(infoCar,JSON.stringify(car));
-// }
-// btn1.onclick = () =>{
-//     goLocalStorage(model.value,type.value,volume.value);
-// }
-//
+let form1 = document.createElement('form');
+let model = document.createElement('input');
+let type = document.createElement('input');
+let volume = document.createElement('input');
+let btn1 = document.createElement('button');
+btn1.innerText = 'Нажми мене';
+form1.setAttribute('name','form1');
+model.setAttribute('name', 'model');
+type.setAttribute('name', 'type');
+volume.setAttribute('name', 'volume');
+document.body.appendChild(form1);
+form1.appendChild(model);
+form1.appendChild(type);
+form1.appendChild(volume);
+document.body.appendChild(btn1);
+
+let goLocalStorage = function (modelCar,typeCar,volumeCar){
+    let infoCar = 'infoCar';
+    let car =[];
+    car = JSON.parse(localStorage.getItem(infoCar));
+    car.push({modelCar,typeCar,volumeCar});
+
+    localStorage.setItem(infoCar,JSON.stringify(car));
+}
+btn1.onclick = () =>{
+    goLocalStorage(model.value,type.value,volume.value);
+}
+
