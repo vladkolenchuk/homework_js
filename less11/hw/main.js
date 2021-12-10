7da25f6d0bb7af94a906bc5cf9fa28f43be7ae9e
vladkolenchuk <kolenchuk09@gmail.com>
2021-12-10 12:13:05 +0200

    ...

--- less11/hw/main.js
+++ less11/hw/main.js
@@ -48,7 +48,7 @@ document.body.appendChild(btn1);
 
 let goLocalStorage = function (modelCar,typeCar,volumeCar){
     let infoCar = 'infoCar';
-    let car =JSON.parse(localStorage.getItem(infoCar)) || []  ;
+    let car = JSON.parse(localStorage.getItem(infoCar)) || []  ;
     car.push({modelCar,typeCar,volumeCar});
 
     localStorage.setItem(infoCar,JSON.stringify(car));
