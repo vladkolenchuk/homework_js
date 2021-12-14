a74366ce695c0286a4523843cb7ecbf0442135ba
vladkolenchuk <kolenchuk09@gmail.com>
2021-12-14 13:49:43 +0200

    ///

--- mini-project/scripts/post-details.js
+++ mini-project/scripts/post-details.js
@@ -3,14 +3,11 @@
 // 8 Ниже информации про пост, вывести все комментарии текущего поста (эндпоинт для получения информации - https://jsonplaceholder.typicode.com/posts/POST_ID/comments)
 
 let postIdInfo = +localStorage.getItem("postIdInfo");
-console.log(postIdInfo);
-
 fetch(`https://jsonplaceholder.typicode.com/posts`)
     .then(response => response.json())
     .then(posts => {
         let wrap = document.createElement('div');
         for (const post of posts) if (post.id === postIdInfo) {
-            console.log(post);
             let blockPost = document.createElement('div');
             blockPost.classList.add('blockPost');
             blockPost.innerHTML = `
@@ -22,27 +19,27 @@ fetch(`https://jsonplaceholder.typicode.com/posts`)
                                                 `;
             let blockComm = document.createElement('div');
             blockComm.innerHTML = `<h3>Коментарии:</h3>`;
-                fetch('https://jsonplaceholder.typicode.com/posts/' + post.id + '/comments')
-                    .then(response => response.json())
-                    .then(comments => {
-                        for (const comment of comments) {
-                            if (post.id === comment.postId) {
-                                let blockCom = document.createElement('div');
-                                blockCom.classList.add('blockCom');
-                                blockCom.innerHTML = `
+            fetch('https://jsonplaceholder.typicode.com/posts/' + post.id + '/comments')
+                .then(response => response.json())
+                .then(comments => {
+                    for (const comment of comments) {
+                        if (post.id === comment.postId) {
+                            let blockCom = document.createElement('div');
+                            blockCom.classList.add('blockCom');
+                            blockCom.innerHTML = `
                                                <p>Id ${comment.id}</p>
                                                <p>Name ${comment.name}</p>
                                                <p>Email ${comment.email}</p>
                                                <p>Body ${comment.body}</p>
                                                `;
-                                blockCom.style.border = ' 1px solid black'
-                                blockPost.appendChild(blockCom)
-                            }
-
+                            blockCom.style.border = ' 1px solid black'
+                            blockPost.appendChild(blockCom)
                         }
-                    })
-            blockPost.appendChild(blockComm);
+
+                    }
+                })
+
             wrap.appendChild(blockPost);
-            }
+        }
         document.body.appendChild(wrap);
-})
+    })
